68e475db8230fe603fdca3c9f8f71d05caf7d35b
BoldFaceType <jtisby@gmail.com>
2026-03-08 11:53:23 -0500

    extract url matrix data loader from viz module

--- viz.js
+++ viz.js
@@ -2,6 +2,7 @@
 
 import * as THREE from 'three'
 import * as util from './util.js'
+import { tryURLInit } from './src/data/url-data-loader.js'
 
 //
 // shader
@@ -85,36 +86,6 @@ const USE_DROPOUT = USE_RANGE.concat(['pt linear'])
 
 export const useRange = name => USE_RANGE.indexOf(name) >= 0
 export const useDropout = name => USE_DROPOUT.indexOf(name) >= 0
-
-const DATA_CACHE = {}
-
-function tryLoadData(data_url) {
-  if (DATA_CACHE[data_url]) {
-    return DATA_CACHE[data_url]
-  }
-  try {
-    console.log(`loading data from ${data_url}...`)
-    const url = new URL(data_url)
-    const req = new XMLHttpRequest()
-    req.open("GET", url, false)
-    req.send(null)
-    DATA_CACHE[url] = req.responseText.split(/\r?\n|\r/).map(l => l.split(',').map(s => +s))
-    console.log(`done loading data from ${data_url}`)
-    return DATA_CACHE[url]
-  } catch (e) {
-    console.log(`error loading data from URL '${data_url}' message '${e.message}`)
-  }
-}
-
-function tryURLInit(url) {
-  const data = tryLoadData(url)
-  if (data) {
-    return (i, j, h, w) => {
-      const row = data[i % data.length]
-      return row[j % row.length]
-    }
-  }
-}
 
 function tryEvalInitExpr(expr) {
   try {
@@ -2101,5 +2072,4 @@ export function genExpr(p) {
   const named = `${p.left.name} @ ${p.right.name}`
   return p.name == expanded || p.name == named ? expanded : `${p.name} = ${expanded}`
 }
-
 
